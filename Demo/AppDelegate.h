eb03e733e4b6e49d2585659483cc2dc136c20894
郭晓倩 <guoxiaoqian@changingedu.com>
2017-02-14 18:14:38 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by 郭晓倩 on 17/2/14.
+//  Copyright © 2017年 郭晓倩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by 郭晓倩 on 17/2/14.
+//  Copyright © 2017年 郭晓倩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
