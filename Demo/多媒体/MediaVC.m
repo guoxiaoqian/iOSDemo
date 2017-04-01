6fb1761aa4cd1c1068a7b735ac636804fe61c522
郭晓倩 <guoxiaoqian@changingedu.com>
2017-04-01 18:44:18 +0800

    拍照

--- Demo/多媒体/MediaVC.m
+++ Demo/多媒体/MediaVC.m
@@ -11,8 +11,9 @@
 #import <AVFoundation/AVFoundation.h> //播放音乐：AVAudioPlayer
 #import <MediaPlayer/MediaPlayer.h>
 #import <FreeStreamer/FreeStreamer.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
-@interface MediaVC ()<AVAudioPlayerDelegate,MPMediaPickerControllerDelegate,AVAudioRecorderDelegate>
+@interface MediaVC ()<AVAudioPlayerDelegate,MPMediaPickerControllerDelegate,AVAudioRecorderDelegate,UINavigationControllerDelegate,UIImagePickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIButton *playButton;
 @property (weak, nonatomic) IBOutlet UISlider *progressBar;
 @property (strong,nonatomic) AVAudioPlayer* audioPlayer;
@@ -27,6 +28,11 @@
 
 @property (strong,nonatomic) FSAudioStream* freeStreamPlayer;
 
+@property (strong,nonatomic) MPMoviePlayerController* moviePlayer;
+
+@property (strong,nonatomic) UIImagePickerController* imagePicker;
+@property (assign,nonatomic) BOOL isVideo;
+
 @end
 
 @implementation MediaVC
@@ -39,6 +45,9 @@
     [self playSoundEffect];
     
     [self setupAVAudioPlayerWithURL:nil];
+    
+    self.moviePlayer.view.frame = CGRectMake(0, 300, kScreenWidth, 200);
+    [self.view addSubview:self.moviePlayer.view];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -396,10 +405,133 @@ void soundCompleteCallback(SystemSoundID soundID,void * clientData){
             [self.freeStreamPlayer play];
             isFirstPlay = NO;
         }else{
+            /**
+             * If the stream is playing, the stream playback is paused upon calling pause.
+             * Otherwise (the stream is paused), calling pause will continue the playback.
+             */
             [self.freeStreamPlayer pause];
         }
         [sender setTitle:@"暂停流音乐" forState:UIControlStateNormal];
     }
+}
+
+#pragma mark - MPMoviePlayerController 支持本地视频和网络视频播放,通过通知回调
+
+-(NSURL *)getNetworkMovieUrl{
+    NSString *urlStr=@"http://192.168.1.161/The New Look of OS X Yosemite.mp4";
+    urlStr=[urlStr stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSURL *url=[NSURL URLWithString:urlStr];
+    return url;
+}
+
+-(MPMoviePlayerController*)moviePlayer{
+    if (!_moviePlayer) {
+        _moviePlayer = [[MPMoviePlayerController alloc] initWithContentURL:[self getNetworkMovieUrl]];
+        [_moviePlayer setShouldAutoplay:NO];
+        _moviePlayer.repeatMode = MPRepeatTypeOne;
+        _moviePlayer.controlStyle = MPMovieControlStyleEmbedded;
+        _moviePlayer.scalingMode = MPMovieScalingModeAspectFit;
+        [_moviePlayer prepareToPlay];
+        
+        //观察通知
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moviePlayerLoadStateChanged:) name:MPMoviePlayerLoadStateDidChangeNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moviePlayerPlaybackStateChanged:) name:MPMoviePlayerPlaybackStateDidChangeNotification object:nil];
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(moviePlayerThumbImageFinish:) name:MPMoviePlayerThumbnailImageRequestDidFinishNotification object:nil]; //缩略图请求完成之后
+        
+    }
+    return _moviePlayer;
+}
+
+-(IBAction)getMovieThumbImage:(id)sender{
+    //    其实使用AVFundation框架中的AVAssetImageGenerator就可以获取视频缩略图。
+    [self.moviePlayer requestThumbnailImagesAtTimes:@[@(2)] timeOption:MPMovieTimeOptionExact];
+}
+
+-(void)moviePlayerLoadStateChanged:(NSNotification*)noti{
+    NSLog(@"moviePlayerLoadStateChanged %ld",self.moviePlayer.loadState);
+}
+-(void)moviePlayerPlaybackStateChanged:(NSNotification*)noti{
+    NSLog(@"moviePlayerPlaybackStateChanged %ld",self.moviePlayer.playbackState);
+    
+}
+-(void)moviePlayerThumbImageFinish:(NSNotification*)noti{
+    NSLog(@"视频截图完成");
+    UIImage* image = noti.userInfo[MPMoviePlayerThumbnailImageKey];
+    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil);
+}
+
+#pragma mark - MPMoviePlayerViewController (MPMoviePlayerController的封装)
+
+//其实MPMoviePlayerController如果不作为嵌入视频来播放（例如在新闻中嵌入一个视频），通常在播放时都是占满一个屏幕的，特别是在iPhone、iTouch上。因此从iOS3.2以后苹果也在思考既然MPMoviePlayerController在使用时通常都是将其视图view添加到另外一个视图控制器中作为子视图，那么何不直接创建一个控制器视图内部创建一个MPMoviePlayerController属性并且默认全屏播放，开发者在开发的时候直接使用这个视图控制器。这个内部有一个MPMoviePlayerController的视图控制器就是MPMoviePlayerViewController
+
+#pragma mark - AVPlayer (比MPMoviePlayerController更底层)
+
+//AVPlayer本身并不能显示视频，而且它也不像MPMoviePlayerController有一个view属性。如果AVPlayer要显示必须创建一个播放器层AVPlayerLayer用于展示，播放器层继承于CALayer，有了AVPlayerLayer之添加到控制器视图的layer中即可。要使用AVPlayer首先了解一下几个常用的类：
+//
+//AVAsset：主要用于获取多媒体信息，是一个抽象类，不能直接使用。
+//
+//AVURLAsset：AVAsset的子类，可以根据一个URL路径创建一个包含媒体信息的AVURLAsset对象。
+//
+//AVPlayerItem：一个媒体资源管理对象，管理者视频的一些基本信息和状态，一个AVPlayerItem对应着一个视频资源。
+
+
+
+//当然AVPlayerItem是有通知的，但是对于获得播放状态和加载状态有用的通知只有一个：播放完成通知AVPlayerItemDidPlayToEndTimeNotification。在播放视频时，特别是播放网络视频往往需要知道视频加载情况、缓冲情况、播放情况，这些信息可以通过KVO监控AVPlayerItem的status、loadedTimeRanges属性来获得。
+
+
+//到目前为止无论是MPMoviePlayerController还是AVPlayer来播放视频都相当强大，但是它也存在着一些不可回避的问题，那就是支持的视频编码格式很有限：H.264、MPEG-4，扩展名（压缩格式）：.mp4、.mov、.m4v、.m2v、.3gp、.3g2等。但是无论是MPMoviePlayerController还是AVPlayer它们都支持绝大多数音频编码，所以大家如果纯粹是为了播放音乐的话也可以考虑使用这两个播放器。那么如何支持更多视频编码格式呢？目前来说主要还是依靠第三方框架，在iOS上常用的视频编码、解码框架有：VLC、ffmpeg
+
+#pragma mark - UIImagePickerController拍照和视频录制
+
+-(UIImagePickerController*)imagePicker{
+    if (!_imagePicker) {
+        _imagePicker = [[UIImagePickerController alloc] init];
+        _imagePicker.delegate = self;
+        _imagePicker.sourceType = UIImagePickerControllerSourceTypeCamera;
+        _imagePicker.showsCameraControls = YES;
+        UIView* overlayView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+        overlayView.backgroundColor = [UIColor yellowColor];
+        _imagePicker.cameraOverlayView = overlayView;
+        _imagePicker.cameraViewTransform = CGAffineTransformMakeRotation(M_PI_4);
+        if([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]){
+            NSLog(@"不支持摄像头");
+        }
+        if (self.isVideo) {
+            _imagePicker.cameraFlashMode = UIImagePickerControllerCameraCaptureModeVideo; //拍照 或 视频
+            _imagePicker.mediaTypes = @[(__bridge id)kUTTypeVideo];//默认kUTTypeImage;kUTTypeMovie带声音,kUTTypeVideo不带
+            _imagePicker.videoQuality = UIImagePickerControllerQualityTypeMedium;
+        }else{
+                    _imagePicker.cameraCaptureMode = UIImagePickerControllerCameraCaptureModePhoto;
+        }
+
+        
+        if([UIImagePickerController isCameraDeviceAvailable:UIImagePickerControllerCameraDeviceFront]){
+            _imagePicker.cameraDevice = UIImagePickerControllerCameraDeviceFront; //前置 或 后置摄像头；UIImagePickerControllerCameraDeviceRear为前置
+        }
+        if([UIImagePickerController isFlashAvailableForCameraDevice:UIImagePickerControllerCameraDeviceFront]){
+        _imagePicker.cameraFlashMode = UIImagePickerControllerCameraFlashModeAuto;//闪光灯
+        }
+    }
+    return _imagePicker;
+}
+
+#pragma mark UIImagePickerControllerDelegate
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info{
+    NSLog(@"imagePickerController:didFinishPickingMediaWithInfo");
+    NSString* mediaType = info[UIImagePickerControllerMediaType];
+    if([mediaType isEqualToString:(__bridge id)kUTTypeImage]){
+        UIImage* image = info[UIImagePickerControllerOriginalImage]; //UIImagePickerControllerEditedImage裁减过的正方形
+    }else if([mediaType isEqualToString:(__bridge id)kUTTypeVideo]){
+        NSURL* videoURL = info[UIImagePickerControllerMediaURL]; //视频存放路径
+        UISaveVideoAtPathToSavedPhotosAlbum(videoURL.absoluteString, nil, nil, nil);
+
+    }
+
+}
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    NSLog(@"imagePickerControllerDidCancel");
 }
 
 @end
