fc68d9b4fb1fc0a1292c094dc159b8e7141b3dba
郭晓倩 <xiaoqianguo@pptv.com>
2017-04-09 16:00:07 +0800

    类方法的动态决议

--- Demo/多媒体/RuntimeVC.m
+++ Demo/多媒体/RuntimeVC.m
@@ -50,17 +50,17 @@ void clasMethodImplemention(id self,SEL _cmd){
         class_addMethod([self class], sel, (IMP)methodImplemention, "v@:");
         return YES;
     }
-    return NO;
+    return [super resolveInstanceMethod:sel];
 }
 
 +(BOOL)resolveClassMethod:(SEL)sel{
-#warning TODO-GUO
-    NSString* selName = NSStringFromSelector(sel);
-    if ([selName isEqualToString:@"classMethodNotImplemented"]) {
-        class_addMethod([self class], sel, (IMP)clasMethodImplemention, "v:");
+    if (sel == @selector(classMethodNotImplemented)) {
+        //参数class必须是元类型，元类中储存的是类对象的元数据，比如类方法就储存在这里。
+        // object_getClass(obj)返回的是obj中的isa指针；而[obj class]则分两种情况：一是当obj为实例对象时，[obj class]中class是实例方法：- (Class)class，返回的obj对象中的isa指针；二是当obj为类对象（包括元类和根类以及根元类）时，调用的是类方法：+ (Class)class，返回的结果为其本身。
+        class_addMethod(object_getClass([self class]), sel, (IMP)clasMethodImplemention, "v@:");
         return YES;
     }
-    return NO;
+    return [super resolveClassMethod:sel];
 }
 
 -(id)forwardingTargetForSelector:(SEL)aSelector{
@@ -68,42 +68,66 @@ void clasMethodImplemention(id self,SEL _cmd){
     if ([selName isEqualToString:@"methodNotImplemented2"]) {
         return [[MessageDeliverBackup alloc] init];
     }
-    return nil;
+    return [super forwardingTargetForSelector:aSelector];
 }
 
 - (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector{
-    //当forwardingTargetForSelector返回nil时调用；结果给forwardInvocation用
-    //未定义或未实现的都返回nil
-    //返回nil直接抛异常：unrecognized selector sent to instance
     NSString* selName = NSStringFromSelector(aSelector);
     if ([selName isEqualToString:@"methodNotImplemented3"]) {
         NSMethodSignature* sig = [NSMethodSignature signatureWithObjCTypes:"v@:"];
         return sig;
     }
-    return nil;
+    return [super methodSignatureForSelector:aSelector];
 }
-
 
 -(void)forwardInvocation:(NSInvocation *)anInvocation{
     NSString* selName = NSStringFromSelector(anInvocation.selector);
     if ([selName isEqualToString:@"methodNotImplemented3"]) {
         NSLog(@"methodNotImplemented3 invoke");
     }else{
-        [self doesNotRecognizeSelector:anInvocation.selector];
+        [super forwardInvocation:anInvocation];
     }
 }
 
--(BOOL)respondsToSelector:(SEL)aSelector{ //需重写,仅调用respondsToSelector时有用
-    NSString* selName = NSStringFromSelector(aSelector);
-    if([selName isEqualToString:@"methodNotImplemented1"] ||
-       [selName isEqualToString:@"methodNotImplemented2"] ||
-       [selName isEqualToString:@"methodNotImplemented3"] ||
-       [selName isEqualToString:@"classMethodNotImplemented"]
-       ){
-        return YES;
-    }
-    return [super respondsToSelector:aSelector];
-}
+//-(BOOL)respondsToSelector:(SEL)aSelector{
+//    //需重写,仅调用respondsToSelector时有用
+//    NSString* selName = NSStringFromSelector(aSelector);
+//    if([selName isEqualToString:@"methodNotImplemented1"] ||
+//       [selName isEqualToString:@"methodNotImplemented2"] ||
+//       [selName isEqualToString:@"methodNotImplemented3"] ||
+//       [selName isEqualToString:@"classMethodNotImplemented"]
+//       ){
+//        return YES;
+//    }
+//    return [super respondsToSelector:aSelector];
+//}
+
+//Objective-C type encodings  (@encode(int))
+//
+//c     A char
+//i     An int
+//s     A short
+//l     A long  l is treated as a 32-bit quantity on 64-bit programs.
+//q     A long long
+//C     An unsigned char
+//I     An unsigned int
+//S     An unsigned short
+//L     An unsigned long
+//Q     An unsigned long long
+//f     A float
+//d     A double
+//B     A C++ bool or a C99 _Bool
+//v     A void
+//*     A character string (char *)
+//@     An object (whether statically typed or typed id)
+//#     A class object (Class)
+//:     A method selector (SEL)
+//[array type]      An array
+//{name=type...}    A structure
+//(name=type...)    A union
+//bnum              A bit field of num bits
+//^type             A pointer to type
+//?         An unknown type (among other things, this code is used for function pointers)
 
 @end
 
@@ -130,18 +154,41 @@ void clasMethodImplemention(id self,SEL _cmd){
 
 #pragma mark - 消息转发
 
+//第一步：先找方法
+//1,首先去该类的方法cache中查找,如果找到了就返回它;
+//2,如果没有找到,就去该类的方法列表中查找。如果在该类的方法列表中找到了,则将 IMP 返回,并将 它加入 cache 中缓存起来。根据最近使用原则,这个方法再次调用的可能性很大,缓存起来可以节省下次 调用再次查找的开销。
+//3,如果在该类的方法列表中没找到对应的IMP,在通过该类结构中的super_class指针在其父类结构的方法列表中去查找,直到在某个父类的方法列表中找到对应的IMP,返回它,并加入cache中;
+//第二步：动态方法决议
+//如果在自身以及所有父类的方法列表中都没有找到对应的 IMP,则看是不是可以进行动态方法决议;
+//resolveInstanceMethod和resolveClassMethod可以为类添加IMP，返回YES表示已决议，否则进入消息转发流程
+//第三部：消息转发
+//1.转发给其他对象：forwardingTargetForSelector，返回self或nil则走第2步
+//2.调用methodSignatureForSelector，获取方法签名（包含方法名、参数、返回类型）， 返回nil直接抛异常：unrecognized selector sent to instance
+//3.通过方法签名构造NSInvocation对象，并调用forwardInvocation，最后一次处理消息的机会。
+//第四部： 报错
+
 -(void)messageDeliver{
     MessageDeliver* deliver = [MessageDeliver new];
-//    [deliver methodNotImplemented1];
-//    [deliver methodNotImplemented2];
-//    [deliver methodNotImplemented3];
-//#pragma clang diagnostic push
-//#pragma clang diagnostic ignored "-Wobjc-method-access"
-//    [deliver methodNotDefined];
-//#pragma clang diagnostic pop
+    [deliver methodNotImplemented1]; //动态方法解析
+    [deliver methodNotImplemented2]; //消息转发第一步
+    [deliver methodNotImplemented3]; //消息转发第二步和第三步
+    
+    [[deliver class] classMethodNotImplemented]; //动态方法解析（类方法）
+}
 
-    [[deliver class] classMethodNotImplemented];
+#pragma mark - 运行时修改属性、方法、类
 
+-(void)runtimeProperty{
+    
+}
+
+-(void)runtimeMethod{
+    
+}
+
+-(void)runtimeClass{
+    
+    
 }
 
 
