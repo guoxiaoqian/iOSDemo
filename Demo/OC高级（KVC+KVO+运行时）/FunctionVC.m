8761ffd26b89ec3ea5f30462436f35afb7588591
郭晓倩 <xiaoqianguo@pptv.com>
2017-07-31 22:25:12 +0800

    算法--分治

--- Demo/OC高级（KVC+KVO+运行时）/FunctionVC.m
+++ Demo/OC高级（KVC+KVO+运行时）/FunctionVC.m
@@ -70,9 +70,9 @@
 @interface FunctionVC ()
 
 @property (strong,nonatomic) NSString* age;
+@property (strong,nonatomic) NSString* name;
 
 @end
-
 
 @implementation FunctionVC
 
@@ -81,11 +81,25 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
-    [self testSynthesize];
+//    [self testSynthesize];
     
-    [self testKVO];
+//    [self testDynamic];
     
-    [self testLoadAndInitialize];
+//    [self testKVO];
+//    
+//    [self testLoadAndInitialize];
+//    
+//    [self testBlock];
+}
+
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    LOG_FUNCTION;
+}
+
+-(void)viewDidAppear:(BOOL)animated{
+    [super viewDidAppear:animated];
+    LOG_FUNCTION;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -103,6 +117,7 @@
 //如果指定访问成员变量_age的话代码如：
 @synthesize age = _age; //只生成_age变量
 
+
 -(void)testSynthesize{
     //获取变量
     unsigned int count = 0;
@@ -113,6 +128,42 @@
     }
     free(varList); //必须释放
 }
+
+
+#pragma mark - Dynamic
+
+//动态不会生成实例变量,也不会生成Set和Get方法，需要运行时自己生成。使用set方法编译时可通过，运行时若没实现就崩溃
+@dynamic name;
+
+-(void)testDynamic{
+    self.name = @"hello";
+    //获取变量
+    unsigned int count = 0;
+    Ivar* varList = class_copyIvarList([self class], &count);
+    for (int i=0; i<count; ++i) {
+        Ivar var = varList[i];
+        NSLog(@"dynamic var name %s",ivar_getName(var));
+    }
+    free(varList); //必须释放
+}
+
+#pragma mark - bridge
+
+-(void)testBridge{
+    NSURL *url = [[NSURL alloc] initWithString:@"http://www.baidu.com"];
+    
+    //仅是类型转换，没有所有权转移
+    CFURLRef ref = (__bridge CFURLRef)url;
+    
+    //NS-->CF所有权转移，需要手动CFRelease
+    ref = (__bridge_retained CFURLRef)url;
+    
+    //CF-->NS所有权转移，由ARC控制计数
+    url = (__bridge_transfer NSURL*)ref;
+}
+
+#pragma mark - KVO
+
 
 -(void)testKVO{
     [self addObserver:self forKeyPath:@"age"
@@ -137,6 +188,52 @@
 //    Category的Initialize会覆盖主类的。
 //    initialize是在第一次主动使用当前类(调用类方法或创建对象)的时候。
 //    即使子类不实现initialize方法，会把父类的实现继承过来调用一遍。
+}
+
+#pragma mark - Block
+
+-(void)testBlock{
+
+    void(^block1)(void) = ^{
+        NSLog(@"Block 1");
+    };
+    
+    NSLog(@"block 未捕获:%@",block1);
+    
+    int a = 0;
+    NSMutableString* string = [[NSMutableString alloc] initWithString:@"郭晓倩"];
+    NSLog(@"block 捕获前 %p %p",&a,&string);
+    void(^block2)(void) = ^{
+        string.string = @"呵呵";
+        NSLog(@"block 捕获中 %p %p",&a,&string);
+    };
+    
+    block2();
+    NSLog(@"block 捕获后:%@ %p",block2,&a);
+
+    __block int b = 1;
+    __block NSMutableString* string2 = [[NSMutableString alloc] initWithString:@"郭晓倩"];
+    NSLog(@"block __block捕获前 %p %p",&b,&string2);
+    void(^block3)(void) = ^{
+        NSLog(@"Block __block捕获中 %p %p",&b,&string2);
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(10 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSLog(@"Block __block捕获中 delay %p",&b);
+        });
+    };
+    block3();
+
+    NSLog(@"block __block捕获后:%@ %p %p",block3,&b,&string2);
+    
+//  在没有捕获任何自动变量的时候, Block 的类型为NSGlobalBlock.
+//  捕获自动变量的时候, Block 的类型为NSStackBlock.
+//  对 Block 在堆上进行复制的时候, 复制后的类型为NSMallocBlock.
+//    
+//  将 Block 作为函数的返回值时, 编译器会自动生成复制到堆上的代码. Blocks 从栈上复制到堆中, 这样即使 Block 的变量作用域结束, 堆上的 Block 还可以继续存在.
+    
+//  在ARC开启的情况下，将只会有NSConcreteGlobalBlock和 NSConcreteMallocBlock类型的block。原本的NSConcreteStackBlock的block会被NSConcreteMallocBlock类型的block替代。
+    
+    //基本类型和对象类型没有__block声明，原始变量一直存储在栈里；ARC下由于是MallocBlock，所以block捕获的变量也存储在堆上,但是不能重新赋值。。
+    //基本类型和对象类型使用__block声明，而没被任何block引用，则原始变量还是存储在栈里；若被block引用了，则原始变量会拷贝到堆上
 }
 
 
---
+++ Demo/算法/algorithm_divide.h
@@ -0,0 +1,11 @@
+//
+//  algorithm_divide.h
+//  Demo
+//
+//  Created by 郭晓倩 on 2017/7/15.
+//  Copyright © 2017年 郭晓倩. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void testDivide();
