485cf06f21bc7a052cb9b73dfb5206e30aee0f08
郭晓倩 <xiaoqianguo@pptv.com>
2018-10-28 15:46:58 +0800

    支持Pod

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '8.0'
+use_frameworks!
+
+source 'git@github.com:guoxiaoqian/SpecsRepo.git'
+
+target 'Demo' do
+	pod 'TTT', '1.0.3'
+end
+
---
+++ Pods/TTT/TTT.framework/Headers/TTT.h
@@ -0,0 +1,19 @@
+//
+//  TTT.h
+//  TTT
+//
+//  Created by 郭晓倩 on 2018/10/28.
+//  Copyright © 2018年 郭晓倩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TTT.
+FOUNDATION_EXPORT double TTTVersionNumber;
+
+//! Project version string for TTT.
+FOUNDATION_EXPORT const unsigned char TTTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TTT/PublicHeader.h>
+
+
